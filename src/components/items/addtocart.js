58953a1e0758aaee10398bd88d76b8c41f8d90ea
Su0414 <susanaik.com>
2018-12-06 21:24:43 -0800

    add layout for addtocart and login page

--- src/components/items/addtocart.js
+++ src/components/items/addtocart.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import { DataTable, TableHeader, Button} from 'react-mdl';
+
 const AddToCart = () => {
         return (
           <div>
-              Add to Cart
+              <h5>Shopping Bag</h5>
+
+              <Button raised colored align="left">Proceed to Checkout</Button>
+
+              <DataTable style={{minWidth: '80%', margin: 'auto'}}
+                shadow={0}
+                rows={[
+                    {
+                      id: 1001, 
+                      item: 'Panash Pink Saree', 
+                      quantity: 2, 
+                      price: 222.90, 
+                      removeItem: 'Delete'
+                    },
+                    {id: 1002, item: 'Kurta', quantity: 5, price: 124.25, removeItem: 'Delete'},
+                    {id: 1003, item: 'Kids Kurta (Gold on Blue)', quantity: 1, price: 24.35, removeItem: 'Delete'}
+                ]}
+              >
+                <TableHeader name="url" tooltip="Image">Product Image</TableHeader>
+
+                <TableHeader name="item" tooltip="The amazing material name">Product</TableHeader>
+                <TableHeader numeric name="quantity" tooltip="Number of materials">Quantity</TableHeader>
+                <TableHeader numeric name="price" cellFormatter={(price) => `$${price.toFixed(2)}`} tooltip="Price pet unit">Price</TableHeader>
+                <TableHeader numeric name="removeItem" tooltip="Remove from bag">Remove</TableHeader>
+            </DataTable>
+<br/>
+            <DataTable style={{minWidth: '80%', margin: 'auto'}}
+                shadow={0}
+                rows={[
+                    {
+                      id: 1001, 
+                      item: 'Womens Kurta', 
+                      quantity: 25, 
+                      price: 2.90
+                    }
+                ]}
+              >
+              <TableHeader name="total" tooltip="Check your total">TOTAL</TableHeader>
+ 
+            </DataTable>
           </div>
     )
   }
--- src/components/items/login.js
+++ src/components/items/login.js
@@ -1,10 +1,48 @@
 import React from 'react'
-const Login = () => {
+import { Button, Textfield , Grid, Cell} from 'react-mdl';
+
+const Login = (props) => {
+
+      const handleOnSubmit = () => {}
+      const {email, password} = props
+
         return (
           <div>
-              UserName
-              Password
-              
+            <Grid className="demo-grid-1">
+              <Cell col={4}></Cell>
+              <Cell col={4}>
+              <h2>Login</h2>
+            <form onSubmit={event => {this.handleOnSubmit(event)}} >
+                <div>
+                <label htmlFor="email">Email</label>
+                      <Textfield
+                          onChange={() => {}}
+                          label="................................."
+                          name="email"
+                          value={email}
+                          style={{width: '200px'}}
+                      />
+                      
+                       
+                </div>      
+                <div>
+                <label htmlFor="password">Password</label>
+                      <Textfield
+                          onChange={() => {}}
+                          label=".................................."
+                          name="password"
+                          value={password}
+                          style={{width: '200px'}}
+                      />
+                     
+                </div>
+                
+              <Button type="submit">Login</Button>
+            </form>
+              </Cell>
+              <Cell col={4}></Cell>
+            </Grid>
+            
           </div>
     )
   }
